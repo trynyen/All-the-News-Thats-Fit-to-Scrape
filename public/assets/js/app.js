0511905cf83f6dfbb8cf56847f29b1fb11c2ef88
Trinh Nguyen <blossom90vt@hotmail.com>
2019-07-25 14:28:28 -0500

    fixed article rendering, and note boxes

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,11 +1,11 @@
 // Grab the articles as a json
-$.getJSON("/articles", function(data) {
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-    }
-  });
+// $.getJSON("/articles", function(data) {
+//     // For each one
+//     for (var i = 0; i < data.length; i++) {
+//       // Display the apropos information on the page
+//       $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+//     }
+//   });
   
   
   // Whenever someone clicks a p tag
@@ -24,20 +24,20 @@ $.getJSON("/articles", function(data) {
       .then(function(data) {
         console.log(data);
         // The title of the article
-        $("#notes").append("<h2>" + data.title + "</h2>");
+        $("#notes").append("<h2>" + data[0].title + "</h2>");
         // An input to enter a new title
         $("#notes").append("<input id='titleinput' name='title' >");
         // A textarea to add a new note body
         $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
         // A button to submit a new note, with the id of the article saved to it
-        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+        $("#notes").append("<button data-id='" + data[0]._id + "' id='savenote'>Save Note</button>");
   
         // If there's a note in the article
-        if (data.note) {
+        if (data[0].note) {
           // Place the title of the note in the title input
-          $("#titleinput").val(data.note.title);
+          $("#titleinput").val(data[0].note.title);
           // Place the body of the note in the body textarea
-          $("#bodyinput").val(data.note.body);
+          $("#bodyinput").val(data[0].note.body);
         }
       });
   });
--- server.js
+++ server.js
@@ -35,72 +35,102 @@ app.set("view engine", "handlebars");
 mongoose.connect("mongodb://localhost/newsdb", { useNewUrlParser: true });
 
 // Routes
-app.get("/", function(req,res){
-   res.send("Welcome to Florida Man Times")
+app.get("/", function (req, res) {
+    res.send("Welcome to Florida Man Times")
 })
 
 // A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
+app.get("/scrape", function (req, res) {
     // First, we grab the body of the html with axios
-    axios.get("https://www.reddit.com/r/FloridaMan/").then(function(response) {
-      // Then, we load that into cheerio and save it to $ for a shorthand selector
-      var $ = cheerio.load(response.data);
-        console.log($);
-      // Now, we grab every h2 within an article tag, and do the following:
-      $("article").each(function(i, element) {
-        // Save an empty result object
-        var result = [];
-        var title = $(element).children().text();
-        // console.log(title);
-        var link = $(element).find("a").attr("href");
-        // console.log(link);
-        result.push({
-            title: title,
-            link: link
-          });
-        
-        // Add the text and href of every link, and save them as properties of the result object
-        // result.title = $(this)
-        //   .children("h3")
-        //   .text();
-        // result.link = $(this)
-        //   .children("a")
-        //   .attr("href");
-        console.log(result);
-        // Create a new Article using the `result` object built from scraping
-        db.Article.create(result)
-          .then(function(dbArticle) {
-            // View the added result in the console
-            console.log(dbArticle);
-          })
-          .catch(function(err) {
-            // If an error occurred, log it
-            console.log(err);
-          });
-      });
-  
-      // Send a message to the client
-      res.send("Scrape Complete");
-    });
-  });
+    axios.get("https://www.reddit.com/r/FloridaMan/").then(function (response) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(response.data);
+        console.log("Num of articles:", $("article").length);
+        // Now, we grab every h2 within an article tag, and do the following:
+        $("article").each(function (i, element) {
+            // Save an empty result object
+            var result = [];
+            var title = $(element).find("h3").text();
+            // console.log(title);
+            var link = $(element).find("a._3jOxDPIQ0KaOWpzvSQo-1s").attr("href");
+            // console.log(link);
+            result.push({
+                title: title,
+                link: link
+            });
 
-  // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
+            // Add the text and href of every link, and save them as properties of the result object
+            // result.title = $(this)
+            //   .children("h3")
+            //   .text();
+            // result.link = $(this)
+            //   .children("a")
+            //   .attr("href");
+            console.log(result);
+            // Create a new Article using the `result` object built from scraping
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    // View the added result in the console
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    // If an error occurred, log it
+                    console.log(err);
+                });
+        });
+
+        // Send a message to the client
+        res.send("Scrape Complete");
+    });
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function (req, res) {
     // Grab every document in the Articles collection
     db.Article.find({})
-      .then(function(dbArticle) {
-        // If we were able to successfully find Articles, send them back to the client
-        res.render("index", {dbArticle:dbArticle});
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
+        .then(function (dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.render("index", { dbArticle: dbArticle });
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+app.get("/articles/:id", function (req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({ _id: req.params.id })
+        .populate("note")
+        .then(function (dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+app.post("/articles/:id", function (req, res) {
+    db.Note.create(req.body)
+    .then(function(dbNote){
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    // Grab every document in the Articles collection
+        .then(function (dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
 });
 // Connect to the Mongo DB
